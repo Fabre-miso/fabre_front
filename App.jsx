c5c3831173cef7b5991e4b782ab4084e25d6a7f4
admin <dlsdn410@sungkyul.ac.kr>
2025-04-02 15:56:02 +0900

    커뮤니티 상세화면, 회원가입 창 수정,커뮤니티 창 구현

--- App.jsx
+++ App.jsx
@@ -7,7 +7,8 @@ import { Image } from 'react-native';
 import HomeMain from './src/screens/HomeMain';
 import SignUpScreen from './src/screens/SignUpScreen';
 import LoginMain from './src/screens/LoginMain';
-import Screen1 from './src/screens/Screen1';
+import CommunityMain from './src/screens/CommunityMain';
+import CommunityDetail from './src/screens/CommunityDetail';
 import Screen2 from './src/screens/Screen2';
 import Screen3 from './src/screens/Screen3';
 import Screen4 from './src/screens/Screen4';
@@ -19,58 +20,57 @@ const BottomTab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-  <BottomTab.Navigator
-  initialRouteName="홈"
-    screenOptions={({ route }) => ({
-      tabBarShowLabel: false,
-      tabBarStyle: {
-        height: 90,
-        borderTopWidth: 0,
-        elevation: 0,
-        backgroundColor: '#fff',
-      },
-      tabBarIcon: ({ color, focused }) => {
-        const tab = tabScreens.find(t => t.name === route.name);
-        const isHome = route.name === '홈';
-      
-        if (!tab) return null;
-      
-        if (isHome) {
-          const homeIcon = focused
-            ? require('./src/assets/image/home_green.png')
-            : require('./src/assets/image/home_gray.png');
-      
+    <BottomTab.Navigator
+      initialRouteName="홈"
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 90,
+          borderTopWidth: 0,
+          elevation: 0,
+          backgroundColor: '#fff',
+        },
+        tabBarIcon: ({ color, focused }) => {
+          const tab = tabScreens.find(t => t.name === route.name);
+          const isHome = route.name === '홈';
+
+          if (!tab) return null;
+
+          if (isHome) {
+            const homeIcon = focused
+              ? require('./src/assets/image/home_green.png')
+              : require('./src/assets/image/home_gray.png');
+
+            return (
+              <Image
+                source={homeIcon}
+                style={{
+                  width: 60,
+                  height: 60,
+                  marginTop: 10,
+                }}
+                resizeMode="contain"
+              />
+            );
+          }
           return (
             <Image
-              source={homeIcon}
+              source={tab.icon}
               style={{
-                width: 60,
-                height: 60,
-                marginTop: 10,
+                width: 24,
+                height: 24,
+                tintColor: color,
+                marginTop: 35,
               }}
               resizeMode="contain"
             />
           );
-        }
-        return (
-          <Image
-            source={tab.icon}
-            style={{
-              width: 24,
-              height: 24,
-              tintColor: color,
-              marginTop: 35,
-            }}
-            resizeMode="contain"
-          />
-        );
-      },
-      
-      tabBarActiveTintColor: '#68936F',
-      tabBarInactiveTintColor: 'gray',
-    })}
-  >
-  
+        },
+
+        tabBarActiveTintColor: '#68936F',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       {tabScreens.map(screen => (
         <BottomTab.Screen
           key={screen.name}
@@ -85,8 +85,8 @@ const TabNavigation = () => {
 
 const tabScreens = [
   {
-    name: '메뉴1',
-    component: Screen1,
+    name: '커뮤니티',
+    component: CommunityMain,
     icon: require('./src/assets/image/screen1.png'),
   },
   {
@@ -122,6 +122,7 @@ const App = () => {
         <Stack.Screen name="PetRegister" component={PetRegister} />
         <Stack.Screen name="PetEdit" component={PetEdit} />
         <Stack.Screen name="SettingScreen" component={SettingScreen} />
+        <Stack.Screen name="CommunityDetail" component={CommunityDetail} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
